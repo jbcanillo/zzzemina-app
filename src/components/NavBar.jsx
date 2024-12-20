7726651192a020ba2aad447a5f781957b18e2380
jbcanillo <jbcanillo@gmail.com>
2024-12-20 14:17:00 +0800

    Made Navbar menu as drawer

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
+import { FaTimes, FaBars } from "react-icons/fa";
 import { useState } from "react";
 
 const NavBar = () => {
@@ -13,26 +14,46 @@ const NavBar = () => {
   };
 
   return (
-    <div className="navbar bg-base-100 shadow-lg fixed w-full z-10" id="app-nav-bar">
-      <div className="flex-1">
-        <Link to="/" className="btn btn-ghost rounded-none text-xl skeleton">Zzzemina</Link>
+    <div
+      className="navbar bg-base-100 shadow-lg fixed w-full z-10"
+      id="app-nav-bar"
+    >
+      <div className="flex-1 ml-16">
+        <Link to="/" className="btn btn-ghost rounded-none text-xl skeleton">
+          Zzzemina
+        </Link>
       </div>
 
       {/* Hamburger menu button (mobile) */}
-      <div className="flex-none lg:hidden">
+      <div className="absolute left-4 top-1/2 transform -translate-y-1/2">
         <button
           onClick={() => setIsMenuOpen(!isMenuOpen)}
           className="btn btn-ghost btn-circle text-xl"
         >
-          <i className={`fas ${isMenuOpen ? 'fa-times' : 'fa-bars'}`}></i> {/* Toggle icon */}
+          <FaBars />
         </button>
       </div>
 
-      {/* Menu (desktop) */}
-      <div className={`flex-none lg:flex ${isMenuOpen ? 'block' : 'hidden'} lg:block`}>
-        <ul className="menu menu-horizontal px-1">
+      {/* Drawer (mobile) */}
+      <div
+        className={`fixed top-0 left-0 h-full bg-base-300 w-64 opacity-90 transform ${
+          isMenuOpen ? "translate-x-0" : "-translate-x-full"
+        } transition-transform duration-300 ease-in-out z-20`}
+      >
+        <div className="absolute top-4 right-4">
+          <button
+            onClick={() => setIsMenuOpen(false)}
+            className="btn btn-ghost text-xl top-5"
+          >
+            <FaTimes />
+          </button>
+        </div>
+
+        <ul className="menu p-4">
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/" onClick={() => setIsMenuOpen(false)}>
+              Home
+            </Link>
           </li>
 
           {/* Only show Management if authenticated as admin */}
@@ -40,15 +61,30 @@ const NavBar = () => {
             <li>
               <details>
                 <summary>Management</summary>
-                <ul className="bg-base-100 rounded-t-none p-2">
+                <ul className="bg-base-100 p-2">
                   <li>
-                    <Link to="/manage_users">Users</Link>
+                    <Link
+                      to="/manage_users"
+                      onClick={() => setIsMenuOpen(false)}
+                    >
+                      Users
+                    </Link>
                   </li>
                   <li>
-                    <Link to="/manage_seminars">Seminars</Link>
+                    <Link
+                      to="/manage_seminars"
+                      onClick={() => setIsMenuOpen(false)}
+                    >
+                      Seminars
+                    </Link>
                   </li>
                   <li>
-                    <Link to="/manage_bookings">Bookings</Link>
+                    <Link
+                      to="/manage_bookings"
+                      onClick={() => setIsMenuOpen(false)}
+                    >
+                      Bookings
+                    </Link>
                   </li>
                 </ul>
               </details>
@@ -59,10 +95,17 @@ const NavBar = () => {
           {isAuthenticated && user?.role === "user" && (
             <>
               <li>
-                <Link to="/my_bookings">My Bookings</Link>
+                <Link to="/my_bookings" onClick={() => setIsMenuOpen(false)}>
+                  My Bookings
+                </Link>
               </li>
               <li>
-                <Link to="/browse_seminars">Browse Seminars</Link>
+                <Link
+                  to="/browse_seminars"
+                  onClick={() => setIsMenuOpen(false)}
+                >
+                  Browse Seminars
+                </Link>
               </li>
             </>
           )}
@@ -73,17 +116,29 @@ const NavBar = () => {
       <div className="flex-none gap-2">
         {isAuthenticated ? (
           <div className="dropdown dropdown-end">
-            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
               <div className="w-10 rounded-full">
                 <img
                   alt="User"
-                  src={user?.profilePic || "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"}
+                  src={
+                    user?.profilePic ||
+                    "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                  }
                 />
               </div>
             </div>
-            <ul tabIndex={0} className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
               <li>
-                <h4 className="justify-center">Hello, {user?.name || "Human"}</h4>
+                <h4 className="justify-center">
+                  Hello, {user?.name || "Human"}
+                </h4>
                 <hr />
               </li>
               <li>
